c294efc9930b6aefa494f9e9a9c62e763b75aeac
BoshraJaber <boshrajaber94@gmail.com>
2021-10-23 17:32:54 +0300

    finished deo

--- README.md
+++ README.md
@@ -10,17 +10,32 @@
 ![](https://d1jnx9ba8s6j9r.cloudfront.net/blog/wp-content/uploads/2018/11/Untitled-1-768x183.jpg)
 
 # Library used:
-* bs4
-* beautiful soup from bs4
+* requests
+* beautifulsoup4
+* json
 
 ## how to asset if a website is good for scraping or not 
 1. if there is a repeated info then It is better to web scrap it then to copy and paste it
 
 ## Steps to scraping:
 1. Get the HTML: define the website and its elements
- * send http request in python
+ * send http request in python using **requests** HTTP library
 2. Parse HTML for element with its class
- * HTML parser (parsing is making sense of something)
+ * HTML parser (parsing is making sense of something) with Beautiful Soup library
 3. Get text from h2 saved as title
 4. Get text from div
 5. save it as description
+
+## How to code it:
+1. `response = requests.get(bayt_url).text`
+2.  `soup = BeautifulSoup(response, 'html.parser') `
+    `result = soup.find('div', id='results_inner_card')`
+    `jobs = result.find_all('li', class_='has-pointer-d')`
+3. Loop over jobs to get what we want : ` title = job.find('h2').text.strip()`
+4. Creating a dictionary: `cleaned_jobs = [clean_job(job) for job in jobs]` , calling the function for each element
+5. dealing with the data: stoing it in a json file: `json_data = json.dumps(cleaned_jobs)`
+
+
+## Useful Sources:
+* [Requests: HTTP Library for Humans](https://docs.python-requests.org/en/latest/)
+* [Beautiful Soup](https://beautiful-soup-4.readthedocs.io/en/latest/)
--- demoWebScraping.py
+++ demoWebScraping.py
@@ -1,21 +1,26 @@
-# Demo: scraping Bayt webapp
+# Demo: scraping Bayt web app
 
 import requests
 from bs4 import BeautifulSoup
-
+######## step 1 ###############
 domain ='https://www.bayt.com'
 bayt_url = f'{domain}/en/jordan/jobs/python-jobs/'
 response = requests.get(bayt_url)
 
 # print(response.headers)
+# response.content , response.status_code, response.headers
 
+html_text = response.text # will print the html page as it is
 
-
-html_text = response.text
+# The following code will create a file with the content
 # with open("file.html",'w') as file:
 #     file.write(html_text)
 # print(html_text)
+
+
+######## step 2 ###############
 soup = BeautifulSoup(html_text, 'html.parser')
+# print(soup.prettify())
 result = soup.find('div', id='results_inner_card')
 jobs = result.find_all('li', class_='has-pointer-d')
 # print(len(jobs))
@@ -41,21 +46,23 @@ jobs = result.find_all('li', class_='has-pointer-d')
 #     cleaned_jobs.append(cleaned_job)
 
 def clean_job(dirty_job):
-    title = dirty_job.find('h2').text.strip()
+    title = dirty_job.find('h2').text.strip() # we use strip to remove any spaces
     company = dirty_job.find('b').text.strip()
     desc = dirty_job.find('p').text.strip()
     link = f"{domain}{dirty_job.find('a').get('href')}"
     return {"title":title, 'company':company, 'desc':desc, 'link':link}
 
+######## step 3 ###############
 cleaned_jobs = [clean_job(job) for job in jobs]
 
 print(cleaned_jobs)
 
 import json
 
+######## step 4 ###############
 json_data = json.dumps(cleaned_jobs)
-print(json_data)
+# print(json_data)
+
+######## step 5 ###############
 with open('json_data.json','w') as file:
     file.write(json_data)
-
-
